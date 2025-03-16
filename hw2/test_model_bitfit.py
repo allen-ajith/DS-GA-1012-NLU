b9cc504016593479814395f7448ec97a87f662b6
allen-ajith <allen200118@gmail.com>
2025-03-16 16:12:17 -0400

    copies of python files

---
+++ hw2/test_model_bitfit.py
@@ -0,0 +1,69 @@
+"""
+Code for Problem 1 of HW 2.
+"""
+import pickle
+import numpy as np
+
+import evaluate
+from datasets import load_dataset
+from transformers import BertTokenizerFast, BertForSequenceClassification, \
+    Trainer, TrainingArguments
+
+from train_model import preprocess_dataset
+
+def compute_metrics(eval_pred):
+    accuracy = evaluate.load("accuracy")
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return accuracy.compute(predictions=predictions, references=labels)
+
+
+def init_tester(directory: str) -> Trainer:
+    """
+    Prolem 2b: Implement this function.
+
+    Creates a Trainer object that will be used to test a fine-tuned
+    model on the IMDb test set. The Trainer should fulfill the criteria
+    listed in the problem set.
+
+    :param directory: The directory where the model being tested is
+        saved
+    :return: A Trainer used for testing
+    """
+
+    model = BertForSequenceClassification.from_pretrained(directory)
+
+    training_args = TrainingArguments(
+        output_dir="test_results",
+        per_device_eval_batch_size=8,
+        do_train=False,
+        do_eval=True,
+        evaluation_strategy="no",
+    )
+
+    return Trainer(
+        model=model,
+        args=training_args,
+        compute_metrics=compute_metrics
+    )
+
+
+if __name__ == "__main__":  # Use this script to test your model
+    model_name = "prajjwal1/bert-tiny"
+
+    # Load IMDb dataset
+    imdb = load_dataset("imdb")
+    del imdb["train"]
+    del imdb["unsupervised"]
+
+    # Preprocess the dataset for the tester
+    tokenizer = BertTokenizerFast.from_pretrained(model_name)
+    imdb["test"] = preprocess_dataset(imdb["test"], tokenizer)
+
+    # Set up tester
+    tester = init_tester("path_to_your_best_model")
+
+    # Test
+    results = tester.predict(imdb["test"])
+    with open("test_results.p", "wb") as f:
+        pickle.dump(results, f)
---
+++ hw2/train_model_bitfit.py
@@ -0,0 +1,179 @@
+"""
+Code for Problem 1 of HW 2.
+"""
+import pickle
+from typing import Any, Dict
+
+import evaluate
+import numpy as np
+import optuna
+from datasets import Dataset, load_dataset
+from transformers import BertTokenizerFast, BertForSequenceClassification, \
+    Trainer, TrainingArguments, EvalPrediction
+
+from transformers import EarlyStoppingCallback
+
+
+
+def preprocess_dataset(dataset: Dataset, tokenizer: BertTokenizerFast) \
+        -> Dataset:
+    """
+    Problem 1d: Implement this function.
+
+    Preprocesses a dataset using a Hugging Face Tokenizer and prepares
+    it for use in a Hugging Face Trainer.
+
+    :param dataset: A dataset
+    :param tokenizer: A tokenizer
+    :return: The dataset, prepreprocessed using the tokenizer
+    """
+
+    MAX_INPUT_LENGTH = 512 #From BERT
+
+    def tokenfn(ex):
+        return tokenizer(
+            ex['text'],  
+            padding=True,
+            truncation=True,
+            max_length=MAX_INPUT_LENGTH,
+            # return_tensors='pt',
+            return_attention_mask = True,
+            return_token_type_ids=True,
+        )
+    return dataset.map(tokenfn, batched=True)
+    
+
+
+def init_model(trial: Any, model_name: str, use_bitfit: bool = False) -> \
+        BertForSequenceClassification:
+    """
+    Problem 2a: Implement this function.
+
+    This function should be passed to your Trainer's model_init keyword
+    argument. It will be used by the Trainer to initialize a new model
+    for each hyperparameter tuning trial. Your implementation of this
+    function should support training with BitFit by freezing all non-
+    bias parameters of the initialized model.
+
+    :param trial: This parameter is required by the Trainer, but it will
+        not be used for this problem. Please ignore it
+    :param model_name: The identifier listed in the Hugging Face Model
+        Hub for the pre-trained model that will be loaded
+    :param use_bitfit: If True, then all parameters will be frozen other
+        than bias terms
+    :return: A newly initialized pre-trained Transformer classifier
+    """
+
+    model = BertForSequenceClassification.from_pretrained(model_name)
+    if use_bitfit:
+        for name, param in model.named_parameters():
+            if "bias" not in name:
+                param.requires_grad = False
+
+    return model
+
+
+def init_trainer(model_name: str, train_data: Dataset, val_data: Dataset,
+                 use_bitfit: bool = False) -> Trainer:
+    """
+    Prolem 2b: Implement this function.
+
+    Creates a Trainer object that will be used to fine-tune a BERT-tiny
+    model on the IMDb dataset. The Trainer should fulfill the criteria
+    listed in the problem set.
+
+    :param model_name: The identifier listed in the Hugging Face Model
+        Hub for the pre-trained model that will be fine-tuned
+    :param train_data: The training data used to fine-tune the model
+    :param val_data: The validation data used for hyperparameter tuning
+    :param use_bitfit: If True, then all parameters will be frozen other
+        than bias terms
+    :return: A Trainer used for training
+    """
+
+    def compute_metrics(eval_pred):
+        accuracy = evaluate.load("accuracy")
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return accuracy.compute(predictions=predictions, references=labels)
+
+    training_args = TrainingArguments(
+        output_dir="checkpoints_bitfit",
+        per_device_train_batch_size=8,    
+        per_device_eval_batch_size = 8,
+        learning_rate=3e-4,                
+        num_train_epochs=4,                        
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+    )
+
+    def model_init(trial=None):
+        return init_model(trial, model_name, use_bitfit)
+
+    trainer = Trainer(
+        model_init=model_init,
+        args=training_args,
+        train_dataset=train_data,
+        eval_dataset=val_data,
+        compute_metrics=compute_metrics,
+    )
+    return trainer 
+
+
+def hyperparameter_search_settings() -> Dict[str, Any]:
+    """
+    Problem 2c: Implement this function.
+
+    Returns keyword arguments passed to Trainer.hyperparameter_search.
+    Your hyperparameter search must satisfy the criteria listed in the
+    problem set.
+
+    :return: Keyword arguments for Trainer.hyperparameter_search
+    """
+
+    search_space = {
+        "learning_rate": [3e-4, 1e-4, 5e-5, 3e-5],
+        "per_device_train_batch_size": [8, 16, 32, 64, 128],
+    }
+    return {
+        "direction": "maximize", 
+        "compute_objective": lambda metrics: metrics["eval_accuracy"],
+        "n_trials": 20,  
+        "hp_space": lambda trial: {
+            "learning_rate": trial.suggest_categorical(
+                "learning_rate", [3e-4, 1e-4, 5e-5, 3e-5] #  From BERT repo
+            ),
+            "per_device_train_batch_size": trial.suggest_categorical(
+                "per_device_train_batch_size", [8, 16, 32, 64, 128] #  From BERT repo
+            )
+        },
+        "backend": "optuna",
+        "sampler": optuna.samplers.GridSampler(search_space)
+    }
+
+
+if __name__ == "__main__":  # Use this script to train your model
+    model_name = "prajjwal1/bert-tiny"
+
+    # Load IMDb dataset and create validation split
+    imdb = load_dataset("imdb")
+    split = imdb["train"].train_test_split(.2, seed=3463)
+    imdb["train"] = split["train"]
+    imdb["val"] = split["test"]
+    del imdb["unsupervised"]
+    del imdb["test"]
+
+    # Preprocess the dataset for the trainer
+    tokenizer = BertTokenizerFast.from_pretrained(model_name)
+
+    imdb["train"] = preprocess_dataset(imdb["train"], tokenizer)
+    imdb["val"] = preprocess_dataset(imdb["val"], tokenizer)
+
+    # Set up trainer
+    trainer = init_trainer(model_name, imdb["train"], imdb["val"],
+                           use_bitfit=True)
+
+    # Train and save the best hyperparameters
+    best = trainer.hyperparameter_search(**hyperparameter_search_settings())
+    with open("train_results_with_bitfit.p", "wb") as f:
+        pickle.dump(best, f)
